164b0e3b6f98392a35c63c40253d3a9f402ca0a3
Mani <manikandanravi94@gmail.com>
2022-12-31 18:57:24 +0530

    function sample pushed

---
+++ functions.py
@@ -0,0 +1,10 @@
+# sample for function
+
+def greet_user(name):
+    print('Hi, developer '+name)
+    print('who loves coding')
+
+
+print('program start')
+greet_user('mani')
+print('program end')
